177ffc86bb17929aeb98a5f18383f47bff175771
Mohit Yadav <mohit.yadav@xebia.com>
2019-05-06 12:21:27 +0400

    add loader changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css';
 import AppBar from './components/AppBar';
 import Search from './components/Search';
 import ListComponent from './components/ListComponent';
+import GistLoader from './components/GistLoader';
 import { getData } from './helpers/api';
 
 const styles = () => ({
@@ -49,201 +50,6 @@ class App extends PureComponent {
       },
     }));
     const forks = await getData(gist.forks_url);
-
-    // const forks = [
-    //   {
-    //     url: 'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f',
-    //     forks_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/forks',
-    //     commits_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/commits',
-    //     id: '912f0b2868960e7f2406b437c49a6a0f',
-    //     node_id: 'MDQ6R2lzdDkxMmYwYjI4Njg5NjBlN2YyNDA2YjQzN2M0OWE2YTBm',
-    //     git_pull_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     git_push_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     html_url: 'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f',
-    //     files: {},
-    //     public: true,
-    //     created_at: '2019-04-21T16:13:41Z',
-    //     updated_at: '2019-04-21T16:14:26Z',
-    //     description: '',
-    //     comments: 0,
-    //     user: null,
-    //     comments_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/comments',
-    //     owner: {
-    //       login: 'Mohit21GoJs',
-    //       id: 42065586,
-    //       node_id: 'MDQ6VXNlcjQyMDY1NTg2',
-    //       avatar_url: 'https://avatars2.githubusercontent.com/u/42065586?v=4',
-    //       gravatar_id: '',
-    //       url: 'https://api.github.com/users/Mohit21GoJs',
-    //       html_url: 'https://github.com/Mohit21GoJs',
-    //       followers_url: 'https://api.github.com/users/Mohit21GoJs/followers',
-    //       following_url:
-    //         'https://api.github.com/users/Mohit21GoJs/following{/other_user}',
-    //       gists_url: 'https://api.github.com/users/Mohit21GoJs/gists{/gist_id}',
-    //       starred_url:
-    //         'https://api.github.com/users/Mohit21GoJs/starred{/owner}{/repo}',
-    //       subscriptions_url:
-    //         'https://api.github.com/users/Mohit21GoJs/subscriptions',
-    //       organizations_url: 'https://api.github.com/users/Mohit21GoJs/orgs',
-    //       repos_url: 'https://api.github.com/users/Mohit21GoJs/repos',
-    //       events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/events{/privacy}',
-    //       received_events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/received_events',
-    //       type: 'User',
-    //       site_admin: false,
-    //     },
-    //   },
-    //   {
-    //     url: 'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f',
-    //     forks_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/forks',
-    //     commits_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/commits',
-    //     id: '912f0b2868960e7f2406b437c49a6a0f',
-    //     node_id: 'MDQ6R2lzdDkxMmYwYjI4Njg5NjBlN2YyNDA2YjQzN2M0OWE2YTBm',
-    //     git_pull_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     git_push_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     html_url: 'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f',
-    //     files: {},
-    //     public: true,
-    //     created_at: '2019-04-21T16:13:41Z',
-    //     updated_at: '2019-04-21T16:14:26Z',
-    //     description: '',
-    //     comments: 0,
-    //     user: null,
-    //     comments_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/comments',
-    //     owner: {
-    //       login: 'Mohit21GoJs',
-    //       id: 42065586,
-    //       node_id: 'MDQ6VXNlcjQyMDY1NTg2',
-    //       avatar_url: 'https://avatars2.githubusercontent.com/u/42065586?v=4',
-    //       gravatar_id: '',
-    //       url: 'https://api.github.com/users/Mohit21GoJs',
-    //       html_url: 'https://github.com/Mohit21GoJs',
-    //       followers_url: 'https://api.github.com/users/Mohit21GoJs/followers',
-    //       following_url:
-    //         'https://api.github.com/users/Mohit21GoJs/following{/other_user}',
-    //       gists_url: 'https://api.github.com/users/Mohit21GoJs/gists{/gist_id}',
-    //       starred_url:
-    //         'https://api.github.com/users/Mohit21GoJs/starred{/owner}{/repo}',
-    //       subscriptions_url:
-    //         'https://api.github.com/users/Mohit21GoJs/subscriptions',
-    //       organizations_url: 'https://api.github.com/users/Mohit21GoJs/orgs',
-    //       repos_url: 'https://api.github.com/users/Mohit21GoJs/repos',
-    //       events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/events{/privacy}',
-    //       received_events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/received_events',
-    //       type: 'User',
-    //       site_admin: false,
-    //     },
-    //   },
-    //   {
-    //     url: 'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f',
-    //     forks_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/forks',
-    //     commits_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/commits',
-    //     id: '912f0b2868960e7f2406b437c49a6a0f',
-    //     node_id: 'MDQ6R2lzdDkxMmYwYjI4Njg5NjBlN2YyNDA2YjQzN2M0OWE2YTBm',
-    //     git_pull_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     git_push_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     html_url: 'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f',
-    //     files: {},
-    //     public: true,
-    //     created_at: '2019-04-21T16:13:41Z',
-    //     updated_at: '2019-04-21T16:14:26Z',
-    //     description: '',
-    //     comments: 0,
-    //     user: null,
-    //     comments_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/comments',
-    //     owner: {
-    //       login: 'Mohit21GoJs',
-    //       id: 42065586,
-    //       node_id: 'MDQ6VXNlcjQyMDY1NTg2',
-    //       avatar_url: 'https://avatars2.githubusercontent.com/u/42065586?v=4',
-    //       gravatar_id: '',
-    //       url: 'https://api.github.com/users/Mohit21GoJs',
-    //       html_url: 'https://github.com/Mohit21GoJs',
-    //       followers_url: 'https://api.github.com/users/Mohit21GoJs/followers',
-    //       following_url:
-    //         'https://api.github.com/users/Mohit21GoJs/following{/other_user}',
-    //       gists_url: 'https://api.github.com/users/Mohit21GoJs/gists{/gist_id}',
-    //       starred_url:
-    //         'https://api.github.com/users/Mohit21GoJs/starred{/owner}{/repo}',
-    //       subscriptions_url:
-    //         'https://api.github.com/users/Mohit21GoJs/subscriptions',
-    //       organizations_url: 'https://api.github.com/users/Mohit21GoJs/orgs',
-    //       repos_url: 'https://api.github.com/users/Mohit21GoJs/repos',
-    //       events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/events{/privacy}',
-    //       received_events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/received_events',
-    //       type: 'User',
-    //       site_admin: false,
-    //     },
-    //   },
-    //   {
-    //     url: 'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f',
-    //     forks_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/forks',
-    //     commits_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/commits',
-    //     id: '912f0b2868960e7f2406b437c49a6a0f',
-    //     node_id: 'MDQ6R2lzdDkxMmYwYjI4Njg5NjBlN2YyNDA2YjQzN2M0OWE2YTBm',
-    //     git_pull_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     git_push_url:
-    //       'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f.git',
-    //     html_url: 'https://gist.github.com/912f0b2868960e7f2406b437c49a6a0f',
-    //     files: {},
-    //     public: true,
-    //     created_at: '2019-04-21T16:13:41Z',
-    //     updated_at: '2019-04-21T16:14:26Z',
-    //     description: '',
-    //     comments: 0,
-    //     user: null,
-    //     comments_url:
-    //       'https://api.github.com/gists/912f0b2868960e7f2406b437c49a6a0f/comments',
-    //     owner: {
-    //       login: 'Mohit21GoJs',
-    //       id: 42065586,
-    //       node_id: 'MDQ6VXNlcjQyMDY1NTg2',
-    //       avatar_url: 'https://avatars2.githubusercontent.com/u/42065586?v=4',
-    //       gravatar_id: '',
-    //       url: 'https://api.github.com/users/Mohit21GoJs',
-    //       html_url: 'https://github.com/Mohit21GoJs',
-    //       followers_url: 'https://api.github.com/users/Mohit21GoJs/followers',
-    //       following_url:
-    //         'https://api.github.com/users/Mohit21GoJs/following{/other_user}',
-    //       gists_url: 'https://api.github.com/users/Mohit21GoJs/gists{/gist_id}',
-    //       starred_url:
-    //         'https://api.github.com/users/Mohit21GoJs/starred{/owner}{/repo}',
-    //       subscriptions_url:
-    //         'https://api.github.com/users/Mohit21GoJs/subscriptions',
-    //       organizations_url: 'https://api.github.com/users/Mohit21GoJs/orgs',
-    //       repos_url: 'https://api.github.com/users/Mohit21GoJs/repos',
-    //       events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/events{/privacy}',
-    //       received_events_url:
-    //         'https://api.github.com/users/Mohit21GoJs/received_events',
-    //       type: 'User',
-    //       site_admin: false,
-    //     },
-    //   },
-    // ];
     this.setState(prevState => ({
       forksForGists: {
         ...prevState.forksForGists,
@@ -252,35 +58,33 @@ class App extends PureComponent {
     }));
   };
 
-  seachForGists = () => {
+  seachForGists = async () => {
     const url = this.makeGistUrl(this.state.username);
+    let gists = [];
     this.setState({
       loadingGists: true,
       forksForGists: {},
+      gists: [],
     });
-    getData(url)
-      // eslint-disable-next-line arrow-parens
-      .then(data => {
-        let gists = [];
-        if (data) {
-          gists = [...data];
-          this.setState({
-            gists: data,
-            loadingGists: false,
-          });
-        }
-        // eslint-disable-next-line no-debugger
-        debugger;
-        return gists;
-      })
-      .then(gists => Array.isArray(gists) && gists.forEach(this.fetchGistUsers))
+    try {
+      const data = await getData(url);
+      if (data) {
+        gists = [...data];
+        this.setState({
+          gists: data,
+          loadingGists: false,
+        });
+      }
+      Array.isArray(gists) && gists.forEach(this.fetchGistUsers);
+    } catch (e) {
       // eslint-disable-next-line no-console
-      .catch(console.error);
+      console.error('error is', e);
+    }
   };
 
   render() {
     const { classes } = this.props;
-    const { gists, forksForGists } = this.state;
+    const { gists, forksForGists, loadingGists } = this.state;
     // eslint-disable-next-line no-debugger
     debugger;
     return (
@@ -296,6 +100,7 @@ class App extends PureComponent {
           </Grid>
           <Grid item sm={1} />
           <Grid item xs={12} md={6}>
+            {loadingGists ? <GistLoader /> : null}
             {gists && gists.length
               ? gists.map(gist => (
                   // eslint-disable-next-line react/jsx-indent
